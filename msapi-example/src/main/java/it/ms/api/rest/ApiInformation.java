6e4e472b458924890784bfbb4edac61fd193bd0a
mssalvo <salvo.mariniello@gmail.com>
2019-09-01 20:28:28 +0200

    apiInformation

--- msapi-example/src/main/java/it/ms/api/rest/ApiInformation.java
+++ msapi-example/src/main/java/it/ms/api/rest/ApiInformation.java
@@ -6,11 +6,14 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
 import org.json.JSONException;
 
+/**
+ * @author m.salvo salvo.mariniello@gmail.com
+ **/
+
 @Path("/")
-public class NotAuthenticated {
+public class ApiInformation {
 
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
